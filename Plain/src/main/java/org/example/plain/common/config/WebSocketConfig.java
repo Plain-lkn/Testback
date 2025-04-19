f5b628e92188fcb018990320e6a488950232d182
kimsee <ttungseui11@gmail.com>
2025-04-19 13:19:30 +0900

    refector : 일부 에러 수정

--- Plain/src/main/java/org/example/plain/common/config/WebSocketConfig.java
+++ Plain/src/main/java/org/example/plain/common/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
-package org.example.plain.domain.meeting.config;
+package org.example.plain.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.plain.domain.chat.handler.ChatWebSocketHandler;
 import org.example.plain.domain.meeting.handler.MeetingWebSocketHandler;
 import org.example.plain.domain.meeting.interceptor.WebSocketAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
     private final MeetingWebSocketHandler meetingWebSocketHandler;
+    private final ChatWebSocketHandler chatWebSocketHandler;
     private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(meetingWebSocketHandler, "/ws/meeting/{roomId}")
                 .addInterceptors(webSocketAuthInterceptor)
+                .setAllowedOrigins("*");
+
+        registry.addHandler(chatWebSocketHandler, "/ws/chat/{chatId}")
+                //.addInterceptors(webSocketAuthInterceptor)
                 .setAllowedOrigins("*");
     }
 } 
--- Plain/src/main/java/org/example/plain/domain/homework/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.plain.domain.homework.repository;
-
-import org.example.plain.domain.file.entity.FileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileEntity, Integer> {
-}
